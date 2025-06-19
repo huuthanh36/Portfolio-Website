8de0aa3956e93ac8d10515a37040100941ce0b47
Nguyễn Hữu Thành <121446657+huuthanh36@users.noreply.github.com>
2025-06-19 15:42:08 +0700

    Add Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
-  return <h1 className="text-3xl font-bold text-center">Hello world</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
